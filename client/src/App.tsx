0bf4d46fd239c72f662c5b09e63caf4da75dd63f
Christopher Druke <cdrukepike@gmail.com>
2024-09-08 22:47:45 -0400

    Implement framework auth logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,17 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-import Search from './components/Search'
-import BookDetails from './components/BookDetails'
-import Bookshelf from './components/Bookshelf'
-import Signin from './components/Signin'
+import { SecurityTokenProvider } from './contexts/SecurityTokenContext'
 import Router from './components/Router'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Router />
+      <SecurityTokenProvider>
+        <Router />
+      </SecurityTokenProvider>
     </BrowserRouter>
   )
 }
 
 export default App
-
-/*
-<Search />
-
-<BookDetails 
-  title="Horror and the Horror Film" 
-  authors={["Bruce F. Kawin"]}
-  description="'Horror and the Horror Film' is a vivid, compelling, insightful and well-written study of the horror film and its subgenres from 1896 to the present, concentrating on the nature of horror in reality and on film."
-  publisher="Anthem Press"
-  publicationDate={2012} />
-
-<Bookshelf />
-
-<Signin />
-*/
--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -1,17 +1,48 @@
+import { FormEvent, useContext, useState } from "react"
+import { SecurityTokenContext } from "../contexts/SecurityTokenContext"
+import { useNavigate } from "react-router-dom";
+
 function Signin() {
+  /* Auth (Context/Router) */
+  const {signin} = useContext(SecurityTokenContext);
+  const navigate = useNavigate();
+
+  /* Input (State) */
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  /* AJAX validation */
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      console.log("API HERE");
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="signin">
       <h1>Bookstore</h1>
-      <form>
+      <form
+        method="POST"
+        onSubmit={handleSubmit}
+      >
         <div className="user-field">
           <label>Username: </label>
-          <input type="text" />
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
         </div>
         <div className="password-field">
           <label>Password: </label>
-          <input type="password" />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
         </div>
-        <button type="submit">LOG-IN</button>
+        <button type="submit" disabled={isLoading}>Login</button>
       </form>
     </div>
   )
