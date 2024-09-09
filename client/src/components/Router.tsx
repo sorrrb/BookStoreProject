9d3ad0fb8217500b3c175abf35524a1f3f9c194d
Christopher Druke <cdrukepike@gmail.com>
2024-09-08 22:47:26 -0400

    Implement basic route protection & dummy content

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom"
 import ProtectedRoute from "./ProtectedRoute"
 import Search from "./Search"
 import Signin from "./Signin"
+import Bookshelf from "./Bookshelf"
+import BookDetails from "./BookDetails"
 
 function Router() {
   return (
     <Routes>
+
       <Route
         path="/search"
         element={
@@ -14,7 +17,25 @@ function Router() {
           </ProtectedRoute>
         }
       />
-      <Route path="/login" element={<Signin />} />
+
+      <Route path="/" element={<Signin />} />
+
+      <Route path="/book/details" element={
+        <ProtectedRoute>
+          <BookDetails
+            title={"Horror and the Horror Film"}
+            authors={["Bruce F. Kawin"]}
+            description={"'Horror and the Horror Film' is a vivid, compelling, insightful and well-written study of the horror film and its subgenres from 1896 to the present, concentrating on the nature of horror in reality and on film."}
+            publisher={"Anthem Press"}
+            publicationDate={2012} />
+        </ProtectedRoute>
+      }/>
+
+      <Route path="/bookshelf" element={
+        <ProtectedRoute>
+          <Bookshelf />
+        </ProtectedRoute>
+      } />
     </Routes>
   )
 }
