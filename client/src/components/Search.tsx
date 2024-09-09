fbbf56399e318f379a16de4845c270c2f2f1bcdc
Christopher Druke <cdrukepike@gmail.com>
2024-09-09 02:25:44 -0400

    Implement additional AJAX error handling

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
+import { error } from "console";
 
 interface IAPIResponse {
   books: Array<object>;
@@ -20,8 +21,13 @@ function Search() {
         `/api/book/search/${searchInputFormatted}`
       );
       setSearchResults(data.books);
+      setHasError("");
     } catch (e) {
       console.error(e);
+      if (axios.isAxiosError(e) && e.response?.status === 404) {
+        setHasError("404 -- Search results not found");
+      } else setHasError("We're sorry - an unexpected error occured.");
+      setSearchResults(([]));
     }
   }
 
@@ -35,17 +41,29 @@ function Search() {
         </div>
         <div className="books-container">
           {searchResults && searchResults.map((book: any, index: number) => {
+            const authorArray = book.authors;
             return (
               <div className="book-result" key={index}>
                 <img className="book-cover" src={book.imageLinks ? book.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png"} />
                 <div className="book-text">
                   <h1>{book.title}</h1>
-                  <h2>{book.authors}</h2>
+                  <h2>{authorArray && authorArray.map((author: string, index: number) => {
+                    return (index >= 3 ?
+                      "" :
+                      <p key={index}>{author}</p>
+                    );
+                  })}</h2>
                 </div>
               </div>
             )
           })}
         </div>
+        {hasError && (
+          <>
+            <div className="ajax-warning">{hasError}</div>
+            <p className="ajax-alert">{"(Try typing more characters if the query is short)"}</p>
+          </>
+        )}
       </div>
     </div>
   );
